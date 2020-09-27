2c3b6cf673adf996c5e6a1b96a64f443e9ea370f
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-09-27 14:23:43 +0300

    last-commit

--- 001-change-color-background/script.js
+++ 001-change-color-background/script.js
@@ -1,12 +1,11 @@
-//Choosing a random color
 const button = document.querySelector('button')
 const body = document.querySelector('body')
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'black', 'orangered']
-
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
 
 function changeBackground(){
 const colorIndex= parseInt(Math.random()*colors.length)
 body.style.backgroundColor = colors[colorIndex]
 }
+
+body.style.backgroundColor = 'violet'
+button.addEventListener('click', changeBackground)
