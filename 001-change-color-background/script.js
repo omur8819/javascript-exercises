7b9299a0e85c4e8b8dcb339aa5f9e7e72d38fef1
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-09-28 21:14:26 +0300

    project-update

--- 001-change-color-background/script.js
+++ 001-change-color-background/script.js
@@ -1,17 +1,25 @@
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'black', 'orangered']
+// const button = document.querySelector('button')
+// const body = document.querySelector('body')
+// // const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'black', 'orangered']
 
 
-function changeBackground(){
-    var colorRed = Math.floor(Math.random * 255 +1);
-    var colorGreen = Math.floor(Math.random * 255 +1);
-    var colorBlue = Math.floor(Math.random * 255 +1);
+// function changeBackground(){
+//     var colorRed = Math.floor(Math.random * 255 +1);
+//     var colorGreen = Math.floor(Math.random * 255 +1);
+//     var colorBlue = Math.floor(Math.random * 255 +1);
 
 
-// const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
-}
+// // const colorIndex= parseInt(Math.random()*colors.length)
+// body.style.backgroundColor = rgb(colorRed, colorGreen, colorBlue);
+// }
 
-body.style.backgroundColor = rgb(r, g, b);
-button.addEventListener('click', changeBackground)
+// body.style.backgroundColor = "violet";
+// button.addEventListener('click', changeBackground);
+
+var colorRed = parseInt(Math.floor(Math.random * 255 +1));
+var colorGreen = parseInt(Math.floor(Math.random * 255 +1));
+var colorBlue = parseInt(Math.floor(Math.random * 255 +1));
+
+console.log(colorRed);
+console.log(colorGreen);
+console.log(colorBlue);
