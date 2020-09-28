b71b98cff0343e333cecaae1a744afac938ea80c
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-09-28 20:32:51 +0300

    project-update

--- 001-change-color-background/script.js
+++ 001-change-color-background/script.js
@@ -1,6 +1,10 @@
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'black', 'orangered']
+// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'black', 'orangered']
+
+var colorRed = r;
+var colorGreen = g;
+var colorBlue = b;
 
 function changeBackground(){
 const colorIndex= parseInt(Math.random()*colors.length)
